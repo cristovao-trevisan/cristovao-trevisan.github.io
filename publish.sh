f8406f791dae4f46f4c3a760e8dc418e6db46238
Cristóvão Trevisan <cristovao.trevisan@gmail.com>
2019-06-03 23:24:16 -0300

    Fix publish .nojekyll

--- publish.sh
+++ publish.sh
@@ -1,6 +1,13 @@
 rm -rf docs
 npm run export
+
 cp -r __sapper__/export/webpage docs
+
+# add .nojekyll to every docs subfolder
+for D in `find docs -type d`
+do
+  touch $D/.nojekyll
+done
 
 git add .
 git commit -m "Publish to github pages"
