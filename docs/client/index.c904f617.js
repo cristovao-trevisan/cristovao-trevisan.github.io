8180ff04cc16da4b5baeb9a58e12c705eea738c0
Cristóvão Trevisan <cristovao.trevisan@gmail.com>
2019-06-04 03:50:12 -0300

    Publish to github pages

--- docs/client/index.c904f617.js
+++
@@ -1,2 +0,0 @@
-import{S as s,i as t,s as a,t as r,j as c,f as e,n,d as o}from"./chunk.fca4c666.js";function u(s){var t;return{c(){t=r("test")},l(s){t=c(s,"test")},m(s,a){e(s,t,a)},p:n,i:n,o:n,d(s){s&&o(t)}}}export default class extends s{constructor(s){super(),t(this,s,null,u,a,[])}}
-//# sourceMappingURL=index.c904f617.js.map
