69caa17f3f7587649a0bec61b2cff9679c7f646e
Cristóvão Trevisan <cristovao.trevisan@gmail.com>
2019-06-06 17:35:08 -0300

    Set async loading background

--- src/components/gallery/AsyncImage.svelte
+++ src/components/gallery/AsyncImage.svelte
@@ -11,15 +11,26 @@
   onMount(loadImage)
 </script>
 
-<img
-  src={initial}
-  data-src={src}
-  alt={alt}
-  bind:this={image}
-/>
+<div class="container">
+  <img
+    src={initial}
+    data-src={src}
+    alt={alt}
+    bind:this={image}
+  />
+</div>
 
-<style>
+<style lang="scss">
+  @import '../../style/colors.scss';
+  .container {
+    width: 100%;
+    height: 100%;
+    background-color: nth($blue, 3);
+  }
   img {
+    display: flex;
+    justify-content: center;
+    align-items: center;
     object-fit: cover;
     width: 100%;
     height: 100%;
