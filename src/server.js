2add4b846e7d51f64ce4af51a98696edf6c37f3f
Cristóvão Trevisan <cristovao.trevisan@gmail.com>
2019-06-28 17:48:13 -0300

    Go back to polka

--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
 import sirv from 'sirv';
-import express from 'express';
+import polka from 'polka';
 
 import compression from 'compression';
 import * as sapper from '@sapper/server';
@@ -7,14 +7,7 @@ import * as sapper from '@sapper/server';
 const { PORT, NODE_ENV } = process.env;
 const dev = NODE_ENV === 'development';
 
-express()
-	// this will make url base lang work when using the server
-	// .use(
-	// 	'/webpage/lang/:lang*',
-	// 	(req, res) => {
-	// 		res.redirect(req.baseUrl.replace(/\/lang\/[^/]+/, '') + '?lang=' + req.params.lang)
-	// 	},
-	// )
+polka()
 	.use(
 		'/webpage',
 		compression({ threshold: 0 }),
