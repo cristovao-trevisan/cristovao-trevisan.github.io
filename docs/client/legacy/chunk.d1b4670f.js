3d84e85a72f4ed6116b0f8bcdf31f86b09e6b4c5
Cristóvão Trevisan <cristovao.trevisan@gmail.com>
2019-06-28 16:19:42 -0300

    Publish to github pages

--- docs/client/legacy/chunk.d1b4670f.js
+++
@@ -1,2 +0,0 @@
-import{H as a,I as t,V as i,F as e}from"./chunk.baa3a598.js";var l=[{path:"",title:"homePage",icon:a},{path:"gallery",title:"gallery",icon:t},{path:"videos",title:"videos",icon:i},{path:"curriculum",title:"curriculum",icon:e}];export{l as m};
-//# sourceMappingURL=chunk.d1b4670f.js.map
